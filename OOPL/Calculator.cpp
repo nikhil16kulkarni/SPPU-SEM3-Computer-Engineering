dc704fba2b6b8a300ecd5b4d4d611adc3dd9c7c4
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-07-05 14:04:15 +0530

    Update Calculator.cpp

--- OOPL/Calculator.cpp
+++ OOPL/Calculator.cpp
@@ -6,6 +6,21 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+// Problem Statement : 
+//Write a C++ program to create a calculator for an arithmetic operator (+, -,
+//*, /). The program should take two operands from user and performs the
+//operation on those two operands depending upon the operator entered by
+//user. Use a switch statement to select the operation. Finally, display the
+//result.
+//Some sample interaction with the program might look like this:
+//Enter first number, operator, second number: 10 / 3
+//Answer = 3.333333
+//Do another (y/n)? y
+//Enter first number, operator, second number: 12 + 100
+//Answer = 112
+//Do another (y/n)? n
+
+
 #include <iostream>
 using namespace std;
 

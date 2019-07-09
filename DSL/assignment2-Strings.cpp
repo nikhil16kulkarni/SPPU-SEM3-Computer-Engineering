ac79d95e802ae6234ddb0abda2f844f9cfe8a421
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-07-09 13:49:13 +0530

    Update and rename assignment2.cpp to assignment2-Strings.cpp

--- DSL/assignment2-Strings.cpp
+++ DSL/assignment2-Strings.cpp
@@ -4,6 +4,16 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
+
+// Problem statement : Write C++ program with class for String. Write a function  
+//                     Copy, Concatenate, check substring, Equal, Reverse and Length.
+//                     Frequency that determines the frequency of occurrence of particular character in the string.  
+//                     Delete that accepts two integers, start and length. The function computes a new string that is equivalent to the original string, except that length characters being at start have been removed.  
+//                     Chardelete that accepts a character c. The function returns the string with all occurrences of c removed.  
+//                     Replace to make an in-place replacement of a substring w of a string by the string x. note that w may not be of same size of x . 
+//                     Palindrome to check whether given string is palindrome or not
+//                     (Note: Make use of character array to perform all operations)
+
 //============================================================================
 // INCOMPLETE //
 #include <iostream>

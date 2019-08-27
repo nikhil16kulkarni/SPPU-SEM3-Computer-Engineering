e24d6a86a4aa1ee3b5ceb0656a6d60cdbef80538
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-08-27 14:05:54 +0530

    Update assignment4.cpp

--- DSL/assignment4.cpp
+++ DSL/assignment4.cpp
@@ -4,10 +4,17 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
+/* Problem Statement :  Department of Computer Engineering has student's club named 'Pinnacle Club'. Students of Second, third and final year of department can be granted membership on request. Similarly one may cancel the membership of club. First node is reserved for president of club and last node is reserved for secretary of club. Write C++ program to maintain club member‘s information using singly linked list. Store student PRN and Name. Write functions to
+			Add and delete the members as well as president or even secretary.
+			Compute total number of members of club 
+			Display members 
+			Display list in reverse order using recursion 
+			Two linked lists exists for two divisions. Concatenate two lists
+			Merge two sorted singly linked lists.*/
 //============================================================================
 
 
-
+/*INCOMPLETE*/
 
 #include<bits/stdc++.h>
 #include<string>

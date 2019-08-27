9dad1fa13e0a4f5824392f7166aa9e3cf5ab9924
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-08-27 14:08:12 +0530

    Update and rename Assignment5.cpp to Assignment5(b).cpp

--- DSL/Assignment5(b).cpp
+++ DSL/Assignment5(b).cpp
@@ -1,9 +1,13 @@
 //============================================================================
-// Name        : Assignment5.cpp
+// Name        : Assignment5(b).cpp
 // Author      : Nikhil Kulkarni
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
+/* Problem Statement :  The ticket booking system of Cinemax theater has to be implemented using C++ program. There are 10 rows and 7 seats in each row. Doubly circular linked list has to be maintained to keep track of free seats at rows. Assume some random booking to start with. Use array to store pointers (Head pointer) to each row. On demand 
+                        The list of available seats is to be displayed 
+                        The seats are to be booked 
+                        The booking can be cancelled.*/
 //============================================================================
 
 #include <bits/stdc++.h>

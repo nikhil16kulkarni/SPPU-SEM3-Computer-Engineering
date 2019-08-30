bd6698754d711a49694d49057fbee4e1fcd0d7bc
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-08-30 14:35:36 +0530

    Added Problem Statement

--- DSL/Assignment8.cpp
+++ DSL/Assignment8.cpp
@@ -4,6 +4,9 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
+/* Problem Statement :  Implement C++ program for expression conversion as infix to postfix and its evaluation using stack 
+                        based on given conditions:  Operands and operator, both must be single character, 
+                        Input Postfix expression must be in a desired format, Only '+', '-', '*' and '/ ' operators are expected. */
 //============================================================================
 
 #include <bits/stdc++.h>

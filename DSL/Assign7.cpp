faf3d34eed59d1e0bed1f56b15e50569d9b07f14
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-09-06 14:11:05 +0530

    Update Assign7.cpp

--- DSL/Assign7.cpp
+++ DSL/Assign7.cpp
@@ -1,1 +1,97 @@
 
+//============================================================================
+// Name        : Assignment7.cpp
+// Author      : Nikhil Kulkarni
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<bits/stdc++.h>
+using namespace std;
+#define size 10
+
+class stackexp
+{
+    int top;
+    char stk[size];
+public:
+    stackexp()
+    {
+     top=-1;
+    }
+    void push(char);
+    char pop();
+    int isfull();
+    int isempty();
+};
+
+void stackexp::push(char x)
+{
+    top=top+1;
+    stk[top]=x;
+}
+
+char stackexp::pop()
+{
+    char s;
+    s=stk[top];
+    top=top-1;
+    return s;
+}
+
+int stackexp::isfull()
+{
+    if(top==size)
+        return 1;
+    else
+        return 0;
+}
+
+int stackexp::isempty()
+{
+    if(top==-1)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    stackexp s1;
+    char exp[20],ch;
+    int i=0;
+    cout<<"\nEnter the expression";
+    cin>>exp;
+    if((exp[0]==')')||(exp[0]==']')||(exp[0]=='}'))
+    {
+        cout<<"\n Invalid Expression\n";
+        return 0;
+    }
+    else
+    {
+        while(exp[i]!='\0')
+        {
+            ch=exp[i];
+            switch(ch)
+            {
+            case '(':s1.push(ch);break;
+            case '[':s1.push(ch);break;
+            case '{':s1.push(ch);break;
+            case ')':s1.pop();break;
+            case ']':s1.pop();break;
+            case '}':s1.pop();break;
+            }
+            i=i+1;
+        }
+    }
+    if(s1.isempty())
+    {
+        cout<<"\nExpression is well parenthesis\n";
+    }
+    else
+    {
+        cout<<"\nInvalid Expression or not in well parenthesized\n";
+    }
+    return 0;
+}

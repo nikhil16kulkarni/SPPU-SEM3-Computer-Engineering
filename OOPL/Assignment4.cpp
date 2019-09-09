78034b861cab0b17fa8bc4e09297e100265a02a7
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-09-09 13:21:17 +0530

    Update and rename Calculator.cpp to Assignment4.cpp

--- OOPL/Assignment4.cpp
+++ OOPL/Assignment4.cpp
@@ -1,23 +1,21 @@
 //============================================================================
-// Name        : Calculator.cpp
+// Name        : Assignment4.cpp
 // Author      : Nikhil Kulkarni
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 
-// Problem Statement : 
-//Write a C++ program to create a calculator for an arithmetic operator (+, -,
-//*, /). The program should take two operands from user and performs the
-//operation on those two operands depending upon the operator entered by
-//user. Use a switch statement to select the operation. Finally, display the
-//result.
-//Some sample interaction with the program might look like this:
-//Enter first number, operator, second number: 10 / 3
-//Answer = 3.333333
-//Do another (y/n)? y
-//Enter first number, operator, second number: 12 + 100
-//Answer = 112
-//Do another (y/n)? n
+/* Problem Statement :  Write a C++ program to create a calculator for an arithmetic operator (+, -,*, /). The program should take two operands from user and performs the
+			operation on those two operands depending upon the operator entered by
+			user. Use a switch statement to select the operation. Finally, display the
+			result.
+			Some sample interaction with the program might look like this:
+			Enter first number, operator, second number: 10 / 3
+			Answer = 3.333333
+			Do another (y/n)? y
+			Enter first number, operator, second number: 12 + 100
+			Answer = 112
+			Do another (y/n)? */
 
 //============================================================================
 

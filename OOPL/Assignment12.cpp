e2735b10316d17e0c347985c64b19a8dd083ab7c
Nikhil Kulkarni <nikhil16kulkarni@gmail.com>
2019-10-18 07:56:17 +0530

    Update Assignment12.cpp

--- OOPL/Assignment12.cpp
+++ OOPL/Assignment12.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : deque.cpp
-// Author      : kandlagunta rohith
+// Author      : Nikhil Kulkarni
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
